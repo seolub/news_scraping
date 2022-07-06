d1bd73d88a2a56567b2d952fc1c108c917e44988
sergio <sergiou@uchicago.edu>
2022-07-06 06:22:26 -0500

    install_changes

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo -e "Installing the virtual environment..."
+echo -e "Creating new virtual environment..."
 python3 -m venv news_scraping
+echo -e "Installing Requirements..."
 source news_scraping/bin/activate
 pip3 install -r requirements.txt
-deactivate
+
 echo -e "Virtual environment is ready!"
+
+echo -e "Starting application."
+python3 scrape.py
